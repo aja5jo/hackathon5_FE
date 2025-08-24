14b5d3564950401ea0b5584d1af733da8a795d15
Changyubjang <jangchang-yeob@MacBook-Pro-2.local>
2025-08-24 21:39:50 +0900

    0824 4차수정

--- src/layouts/HeaderLayout.jsx
+++ src/layouts/HeaderLayout.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom'
 import Header from '../components/common/Header'
 import ErrorBoundary from '../components/common/ErrorBoundary'
 import styled from 'styled-components';
 
 function HeaderLayout() {
+  const location = useLocation();
+  
   return (
     <LayoutWrapper>
         <Header/>
-        <ContentWrapper id="viewer">
+        <ContentWrapper id="viewer" key={location.pathname}>
             <ErrorBoundary>
                 <Outlet />
             </ErrorBoundary>
@@ -25,6 +27,8 @@ const ContentWrapper = styled.main`
     padding-top: 64px; /* header의 높이만큼 내리기 */
     min-height: calc(100vh - 64px);
     width: 100%;
+    position: relative;
+    overflow: hidden; /* 이전 콘텐츠가 넘치지 않도록 */
 `;
 
 export default HeaderLayout
